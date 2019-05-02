357fefc6af1d4036a903acf84f97de645c8d88d7
vonjobi <mr_doo2@naver.com>
2019-05-03 00:00:15 +0900

    Dataflow With Props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Movie from './Movie';
 
-function App() {
+const movieTitles = [
+  "Matrix",
+  "Full Metal Jacket",
+  "Oldboy",
+  "Star wars"
+]
+
+const movieImages = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiV5iJmB0F488UoqZDXO5zQXBvdFa3h1BIeo-T2CghlE1DRPhy",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/91ONQ8FNHJL._SL1500_.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMFwZdtmOZdOyW-3WYkqk5YyRpXP-W1PDXwDpfZX-U1plfj9FX"
+]
+
+
+
+class App extends Component {
+
+
+  render(){
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Movie title={movieTitles[0]} poster={movieImages[0]}/>
+      <Movie title={movieTitles[1]} poster={movieImages[1]}/>
+      <Movie title={movieTitles[2]} poster={movieImages[2]}/>
+      <Movie title={movieTitles[3]} poster={movieImages[3]}/>
     </div>
   );
+  }
 }
 
 export default App;
