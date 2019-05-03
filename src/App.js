7802073c8dbf944f8a294832fff3551e4c8b1838
vonjobi <mr_doo2@naver.com>
2019-05-03 20:23:18 +0900

    Create MovieList

--- src/App.js
+++ src/App.js
@@ -13,37 +13,31 @@ class App extends Component {
   }
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies : [
-          {
-            title : "Matrix",
-            poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiV5iJmB0F488UoqZDXO5zQXBvdFa3h1BIeo-T2CghlE1DRPhy",
-          },
-          {
-            title : "Full Metal Jacket",
-            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg",
-          },
-          {
-            title : "Oldboy",
-            poster : "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg",
-          },
-          {
-            title : "Star wars",
-            poster : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMFwZdtmOZdOyW-3WYkqk5YyRpXP-W1PDXwDpfZX-U1plfj9FX"
-          }
-        ]
-      })
-    }, 5000
-    )
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
     })
     return movies
   }
+
+  _getMovies = async () =>{
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort by=rating')
+    .then(response=>response.json())
+    .then(json=> json.data.movies)
+    .catch(err=>console.log(err))
+  }
+
 
   render(){
   return (
