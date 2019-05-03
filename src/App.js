6e60d0e8807f7409222c4a1f4e1fddc0d3b25fc3
vonjobi <mr_doo2@naver.com>
2019-05-03 22:59:46 +0900

    Creat Movie App (Final_190503)

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class App extends Component {
   //          -> render() -> componentDidUpdate()
 
   state = {
-  
   }
 
   componentDidMount(){
@@ -18,7 +17,13 @@ class App extends Component {
 
   _renderMovies = () => {
     const movies = this.state.movies.map(movie => {
-      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
+      return <Movie 
+      title={movie.title_english} 
+      poster={movie.large_cover_image} 
+      key={movie.id} 
+      genres={movie.genres}
+      synopsis={movie.synopsis}
+      />
     })
     return movies
   }
@@ -32,17 +37,18 @@ class App extends Component {
   }
 
   _callApi = () => {
-    return fetch('https://yts.am/api/v2/list_movies.json?sort by=rating')
-    .then(response=>response.json())
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    .then(potato=>potato.json())
     .then(json=> json.data.movies)
     .catch(err=>console.log(err))
   }
 
 
   render(){
+    const { movies } = this.state;
   return (
-    <div className="App">
-      {this.state.movies ? this._renderMovies() : 'Loading ... '}
+    <div className={ movies ? "App" : "App--loading"}>
+      {movies ? this._renderMovies() : 'Loading'}
     </div>
   );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,37 +1,59 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types'
+import LinesEllipsis from 'react-lines-ellipsis'
 import './Movie.css';
 
-class Movie extends Component {
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-
-  render(){
-  return (
-    <div>
-        <MoviePoster poster={this.props.poster}/>
-        <h1>{this.props.title}</h1>
-    </div>
-  );
-  }
+function Movie({title, poster, genres, synopsis}){
+    return (
+        <div className="Movie">
+            <div className="Movie__Column">
+                <MoviePoster poster={poster} alt={title} />
+            </div>
+            <div className="Movie__Column">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <div className="Movie__Synopsis">
+                <LinesEllipsis
+                text={synopsis}
+                maxLine='3'
+                ellipsis=' ...'
+                trimRight
+                basedOn='letters'
+                />
+                </div>
+            </div>
+        </div>
+    )
 }
 
+function MoviePoster({poster, alt}){
+    return(
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster"/>
+    )
+}
 
-class MoviePoster extends Component{
+function MovieGenre({genre}){
+    return (
+        <span className="Movie__Genre">{genre}</span>
+    )
+}
 
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres : PropTypes.array.isRequired, 
+    synopsis : PropTypes.string.isRequired
+}
 
-    render(){
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
 
-        return(
-            <img src={this.props.poster} alt="Movie Poster" />
-        )
-    }
+MovieGenre.propTypes = {
+    genre : PropTypes.string.isRequired
 }
 
 export default Movie;
